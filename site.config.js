b2aae111d008cbce122ec46e0038b28ca450e26c
JunYeong <gowjr207@gmail.com>
2023-04-02 01:52:06 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `POOM-backend-code`,
+      href: "https://github.com/lucky-treee/diamond",
     },
   ],
   // blog setting (required)
